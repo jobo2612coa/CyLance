5276062f565dfe635c474eddeb6bea42f4ed2779
Joe Bowen <jobo2612@gmail.com>
2021-01-08 10:29:41 -0600

    1st ver, generated

---
+++ VisioTest/VisioTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisioTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
