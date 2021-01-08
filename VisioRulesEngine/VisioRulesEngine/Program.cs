20f7f5dc5f093d0c2d638e9540511aa9d5a69c90
Joe Bowen <jobo2612@gmail.com>
2021-01-07 23:03:16 -0600

    Testing my link to GitHub

--- VisioRulesEngine/VisioRulesEngine/Program.cs
+++ VisioRulesEngine/VisioRulesEngine/Program.cs
@@ -7,6 +7,7 @@ namespace VisioRulesEngine
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Globe!");
         }
     }
 }
